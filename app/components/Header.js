7a3fcd67153c1202376b0e71e71cfb25cf30e88a
omrobbie <putu.roby@yahoo.com>
2016-11-13 19:20:16 +0800

    * edit header.js agar tombol learn more mengacu ke repo github

--- app/components/Header.js
+++ app/components/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
 				<div className="container">
 					<h1>Simple React-Bootstrap</h1>
 					<p>Membuat kerangka sederhana react dan bootstrap</p>
-					<p><a className="btn btn-primary btn-lg">Learn more</a></p>
+					<p><a className="btn btn-primary btn-lg" href="https://github.com/omrobbie/react-bootstrap">Learn more</a></p>
 				</div>
 			</div>
 		</div>;
