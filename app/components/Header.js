262a7f87542fe2288a5a0b20a59115d5bfcffab9
omrobbie <putu.roby@yahoo.com>
2016-11-13 19:14:34 +0800

    + header.js dengan bootstrap jumbotron

---
+++ app/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+	render() {
+		return <div>
+			<div className="jumbotron">
+				<div className="container">
+					<h1>Simple React-Bootstrap</h1>
+					<p>Membuat kerangka sederhana react dan bootstrap</p>
+					<p><a className="btn btn-primary btn-lg">Learn more</a></p>
+				</div>
+			</div>
+		</div>;
+	}
+}
+
+export default Header;
